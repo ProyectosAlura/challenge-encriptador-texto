c2f39f9df115840561be81659e4e3cd22daf3bb2
Daniel <danielcaro1000@gmail.com>
2022-09-07 20:54:01 -0500

    funcion para estado del area de texto

--- logica.js
+++ logica.js
@@ -1,2 +1,16 @@
 var texto = document.querySelector(".campo-texto");
 var pantalla = document.querySelector(".pantalla");
+var estado = document.querySelector(".estado");
+
+//Funciones
+
+function verificar(){
+    var mensaje = texto.value.toLocaleLowerCase();
+    if(mensaje==="" && pantalla.value===""){
+        estado.style.visibility="visible";
+    }else{
+        estado.style.visibility="hidden";
+    }
+}
+
+setInterval(verificar,400);
