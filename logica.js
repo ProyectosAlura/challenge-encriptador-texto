168f00061939524a8d1c448ba388a92a68f2bf2a
Daniel <danielcaro1000@gmail.com>
2022-09-07 21:39:21 -0500

    funcion encriptar

--- logica.js
+++ logica.js
@@ -13,4 +13,46 @@ function verificar(){
     }
 }
 
+function encriptar(){
+    var mensaje = texto.value.toLocaleLowerCase();
+    console.log(mensaje);
+    console.log("funciona");
+    var iterador = 0;
+    var encriptado="";
+
+    while(iterador<mensaje.length){
+        switch(mensaje[iterador]){
+            case "e":
+                encriptado+="enter";
+                break;
+            
+            case "i":
+                encriptado+="imes";
+                break;
+            
+            case "a":
+                encriptado+="ai";
+                break;
+            
+            case "o":
+                encriptado+="ober";
+                break;
+
+            case "u":
+                encriptado+="ufat";
+                break;
+
+            default:
+                encriptado+=mensaje[iterador];
+                break;
+        }
+        iterador++;
+    }
+    texto.value="";
+    pantalla.innerText=encriptado;
+}
+
 setInterval(verificar,400);
+
+var botonEncriptar = document.querySelector(".boton-encriptar");
+botonEncriptar.onclick=encriptar;
